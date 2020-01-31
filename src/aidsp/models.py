6966c1a31266111b3e851e3eda165a541c0bcd28
徐梓琅 <ksbc24v@gmail.com>
2020-01-31 17:00:12 +0800

    edit models

--- src/aidsp/models.py
+++ src/aidsp/models.py
@@ -5,14 +5,16 @@ class Project(models.Model):
     id = models.AutoField(primary_key=True)
     project_id = models.CharField(max_length=50, verbose_name='项目id')
     project_name = models.CharField(max_length=50, verbose_name='项目名称')
-    #tasks = models.ForeignKey(to='Task', to_field='project', related_name='project', on_delete=models.SET_NULL, verbose_name='任务')
+    # tasks = models.ForeignKey(to='Task', to_field='project', related_name='project', on_delete=models.SET_NULL, verbose_name='任务')
     status = models.CharField(max_length=20, verbose_name='状态')
+    create_time = models.DateTimeField(verbose_name='创建时间')
+    end_time = models.DateTimeField(verbose_name='结束时间')
     background = models.TextField(verbose_name='项目背景', help_text='填写此项目的需求背景，必须是markdown格式')
     total_demand = models.PositiveIntegerField(verbose_name='需求总量')
     total_describe = models.CharField(max_length=200, verbose_name='需求数量描述')
     deadline = models.DateTimeField(verbose_name='截止渐渐')
     documents = models.ManyToManyField(to='Document', related_name='projects', verbose_name='文档')
-    #datasets = models.ManyToManyField(to='Dataset', related_name='project', verbose_name='数据集')
+    # datasets = models.ManyToManyField(to='Dataset', related_name='project', verbose_name='数据集')
     labels = models.ManyToManyField(to='Label', related_name='labels', verbose_name='标签')
     users_found = models.ManyToManyField(to='User', related_name='users_found', verbose_name='创建人')
     users_manager = models.ManyToManyField(to='User', related_name='users_manager', verbose_name='管理人')
@@ -39,3 +41,84 @@ class User(models.Model):
     email = models.EmailField(verbose_name='邮箱')
     position = models.PositiveIntegerField(choices=POSITIONS, verbose_name='职位')
     current_task = models.IntegerField(verbose_name='当前任务')
+    # project_founder project_manager project_attend
+    # tasks = models.ForeignKey()
+    # tasks_review
+    # q_a_Qs
+    # q_a_As
+    # q_a_approvals
+    # performance
+
+    class Mata:
+        verbose_name = verbose_name_plural = '用户'
+
+
+class Performance(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(to='User',
+                             to_field='name',
+                             related_name='user',
+                             verbose_name='姓名',
+                             on_delete=models.SET_NULL)
+    performance = models.CharField(max_length=10, verbose_name='绩效')
+    date = models.DateField(verbose_name='打分日期')
+
+class Task(models.Model):
+    STATUS_NOT_BEGIN = 0
+    STATUS_DOING = 1
+    STATUS_DONE = 2
+    STATUS_PASS = 3
+    STATUS_NOT_PASS = 4
+    STATUS = (
+        (STATUS_NOT_BEGIN, '未开始'),
+        (STATUS_DOING, '正在进行'),
+        (STATUS_DONE, '完成'),
+        (STATUS_PASS, '通过'),
+        (STATUS_NOT_PASS, '未通过'),
+    )
+    id = models.AutoField(primary_key=True)
+    project = models.ForeignKey(to='Project',
+                                to_field='project_id',
+                                related_name='project',
+                                verbose_name='所属项目',
+                                on_delete=models.SET_NULL)
+    create_time = models.DateTimeField(verbose_name='创建时间')
+    task_name = models.CharField(max_length=20, verbose_name='任务名称')
+    task_link = models.URLField(verbose_name='任务链接')
+    begin_time = models.DateTimeField(verbose_name='开始时间')
+    done_time = models.DateTimeField(verbose_name='完成时间')
+    time_label = models.DateTimeField(verbose_name='时间标记点')
+    used_time = models.DateTimeField(verbose_name='任务用时')
+    total_time = models.DateTimeField(verbose_name='任务历时')
+    gross = models.IntegerField(verbose_name='此任务工作总量')
+    quantity_available = models.IntegerField(verbose_name='此任务有效工作量')
+    status = models.PositiveSmallIntegerField(choices=STATUS, verbose_name='状态')
+    number_of_reviews = models.PositiveSmallIntegerField(default=0, verbose_name='审核次数')
+    assignee = models.ForeignKey(to='User',
+                                 to_field='name',
+                                 related_name='assignee',
+                                 verbose_name='标注员',
+                                 on_delete=models.SET_NULL)
+    reviewer = models.ForeignKey(to='User',
+                                 to_field='name',
+                                 related_name='reviewer',
+                                 verbose_name='审核员',
+                                 on_delete=models.SET_NULL)
+    # suggestions = models.TextField(verbose_name='修改建议', help_text='填写此任务的修改建议，必须是markdown格式')
+
+    class Mata:
+        verbose_name = verbose_name_plural = '任务'
+
+
+class Suggestion(models.Model):
+    id = models.AutoField(primary_key=True)
+    task = models.ForeignKey(to='Task',
+                             to_field='task_name',
+                             related_name='task',
+                             verbose_name='任务',
+                             on_delete=models.SET_NULL)
+    index = models.IntegerField(verbose_name='图片序号')
+    problem = models.TextField(verbose_name='问题内容', help_text='填写此图片的问题详情，必须是markdown格式')
+
+    class Mata:
+        verbose_name = verbose_name_plural = '修改建议'
