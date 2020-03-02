f06c255da52769640b1dc7f4834abf55d8201a55
徐梓琅 <ksbc24v@gmail.com>
2020-03-02 12:01:55 +0800

    update urls

--- src/AIDSPworkflow/settings/develop.py
+++ src/AIDSPworkflow/settings/develop.py
@@ -100,16 +100,16 @@ WSGI_APPLICATION = 'AIDSPworkflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aidsp',
-        'USER': 'django_start',
-        'PASSWORD':'django_start',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'aidsp',
+#         'USER': 'django_start',
+#         'PASSWORD':'django_start',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
