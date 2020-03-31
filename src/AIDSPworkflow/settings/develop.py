0f5609b77d3b43c74a4ea27470cd29cd98ed00e3
xhwyzz <sjb123321!>
2020-03-31 10:46:33 +0800

    '添加docker安装'

--- src/AIDSPworkflow/settings/develop.py
+++ src/AIDSPworkflow/settings/develop.py
@@ -98,23 +98,23 @@ WSGI_APPLICATION = 'AIDSPworkflow.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aidsp',
-        'USER': 'django_start',
-        'PASSWORD':'django_start',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+        'PASSWORD': 'postgres',
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':  os.path.join(BASE_DIR, 'db.sqlite3')
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
