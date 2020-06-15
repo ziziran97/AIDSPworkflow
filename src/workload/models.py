1f47db115776066449740b99e3b99098cc67c532
xhwyzz <sjb123321!>
2020-06-15 16:51:23 +0800

    '添加项目内工作量展示'

--- src/workload/models.py
+++ src/workload/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 
 class Workload(models.Model):
-    assignee = models.CharField(max_length=255, blank=True, null=True)
-    updated_date = models.DateTimeField(blank=True, null=True)
-    workcount = models.IntegerField(blank=True, null=True)
-    lastid = models.IntegerField(blank=True, null=True)
+    assignee = models.CharField(max_length=255, blank=True, null=True, verbose_name='标注员')
+    updated_date = models.DateTimeField(blank=True, null=True, verbose_name='创建时间')
+    workcount = models.IntegerField(blank=True, null=True, verbose_name='工作量')
+    lastid = models.IntegerField(blank=True, null=True, verbose_name='最后id')
+    task = models.CharField(max_length=255, blank=True, null=True, verbose_name='任务')
 
     class Meta:
         db_table = 'workload'
